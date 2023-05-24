23f3033f9f3e5d476c9e9a8e7ddc14e0404ea651
jkarlos18 <134089256+jkarlos18@users.noreply.github.com>
2023-05-24 16:14:52 +0000

    mudança

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "josé carlos ",
+  title: "estudante ifpb curso tsi",
+  email: "jose.santos.18@academico.ifpb.edu.br",
+  gitHub: "jkarlos18",
+  instagram: "j.karlos18",
 };
 
 const primaryColor = "#4E567E";
